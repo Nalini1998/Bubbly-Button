9c53ddae4f197527df010da66b053b26fdeb371b
Nalini Vo <131301452+Nalini1998@users.noreply.github.com>
2024-01-05 00:00:22 +0700

    Files uploaded

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Bubbly-Button
+# Bubbly Button
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Nalini1998/pen/dyQEerz/d90047d8057aeee44c2c60527873ec1f](https://codepen.io/Nalini1998/pen/dyQEerz/d90047d8057aeee44c2c60527873ec1f).
+
+Inspired by:  https://dribbble.com/shots/3975065-With-a-Click-of-a-Button  By Gal Shir
+
+Made the bubbles using  "radial-gradient" for background-image.
+ I believe this property is so cool that you can draw many things without adding extra divs or pseudo elements  (::before and ::after)
---
+++ script.js
@@ -0,0 +1,208 @@
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button1");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons2 = document.getElementsByClassName("bubbly-button2");
+
+for (var i = 0; i < bubblyButtons2.length; i++) {
+  bubblyButtons2[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons3 = document.getElementsByClassName("bubbly-button3");
+
+for (var i = 0; i < bubblyButtons3.length; i++) {
+  bubblyButtons3[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons4 = document.getElementsByClassName("bubbly-button4");
+
+for (var i = 0; i < bubblyButtons4.length; i++) {
+  bubblyButtons4[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons5 = document.getElementsByClassName("bubbly-button5");
+
+for (var i = 0; i < bubblyButtons5.length; i++) {
+  bubblyButtons5[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons6 = document.getElementsByClassName("bubbly-button6");
+
+for (var i = 0; i < bubblyButtons6.length; i++) {
+  bubblyButtons6[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons7 = document.getElementsByClassName("bubbly-button7");
+
+for (var i = 0; i < bubblyButtons7.length; i++) {
+  bubblyButtons7[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons8 = document.getElementsByClassName("bubbly-button8");
+
+for (var i = 0; i < bubblyButtons8.length; i++) {
+  bubblyButtons8[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons9 = document.getElementsByClassName("bubbly-button9");
+
+for (var i = 0; i < bubblyButtons9.length; i++) {
+  bubblyButtons9[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons10 = document.getElementsByClassName("bubbly-button10");
+
+for (var i = 0; i < bubblyButtons10.length; i++) {
+  bubblyButtons10[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons11 = document.getElementsByClassName("bubbly-button11");
+
+for (var i = 0; i < bubblyButtons11.length; i++) {
+  bubblyButtons11[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons12 = document.getElementsByClassName("bubbly-button12");
+
+for (var i = 0; i < bubblyButtons12.length; i++) {
+  bubblyButtons12[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons13 = document.getElementsByClassName("bubbly-button13");
+
+for (var i = 0; i < bubblyButtons13.length; i++) {
+  bubblyButtons13[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons14 = document.getElementsByClassName("bubbly-button14");
+
+for (var i = 0; i < bubblyButtons14.length; i++) {
+  bubblyButtons14[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons15 = document.getElementsByClassName("bubbly-button15");
+
+for (var i = 0; i < bubblyButtons15.length; i++) {
+  bubblyButtons15[i].addEventListener('click', animateButton, false);
+}
+
+// 
+
+var bubblyButtons16 = document.getElementsByClassName("bubbly-button15");
+
+for (var i = 0; i < bubblyButtons15.length; i++) {
+  bubblyButtons15[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons16 = document.getElementsByClassName("bubbly-button16");
+
+for (var i = 0; i < bubblyButtons16.length; i++) {
+  bubblyButtons16[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons17 = document.getElementsByClassName("bubbly-button17");
+
+for (var i = 0; i < bubblyButtons17.length; i++) {
+  bubblyButtons17[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons18 = document.getElementsByClassName("bubbly-button18");
+
+for (var i = 0; i < bubblyButtons18.length; i++) {
+  bubblyButtons18[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons19 = document.getElementsByClassName("bubbly-button19");
+
+for (var i = 0; i < bubblyButtons19.length; i++) {
+  bubblyButtons19[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons20 = document.getElementsByClassName("bubbly-button20");
+
+for (var i = 0; i < bubblyButtons20.length; i++) {
+  bubblyButtons20[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons21 = document.getElementsByClassName("bubbly-button21");
+
+for (var i = 0; i < bubblyButtons21.length; i++) {
+  bubblyButtons21[i].addEventListener('click', animateButton, false);
+}
+
+
+// From button 22
+
+var bubblyButtons22 = document.getElementsByClassName("bubbly-button22");
+
+for (var i = 0; i < bubblyButtons22.length; i++) {
+  bubblyButtons22[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons23 = document.getElementsByClassName("bubbly-button23");
+
+for (var i = 0; i < bubblyButtons23.length; i++) {
+  bubblyButtons23[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons24 = document.getElementsByClassName("bubbly-button24");
+
+for (var i = 0; i < bubblyButtons24.length; i++) {
+  bubblyButtons24[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons25 = document.getElementsByClassName("bubbly-button25");
+
+for (var i = 0; i < bubblyButtons25.length; i++) {
+  bubblyButtons25[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons26 = document.getElementsByClassName("bubbly-button26");
+
+for (var i = 0; i < bubblyButtons26.length; i++) {
+  bubblyButtons26[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons27 = document.getElementsByClassName("bubbly-button27");
+
+for (var i = 0; i < bubblyButtons27.length; i++) {
+  bubblyButtons27[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons28 = document.getElementsByClassName("bubbly-button28");
+
+for (var i = 0; i < bubblyButtons27.length; i++) {
+  bubblyButtons28[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons29 = document.getElementsByClassName("bubbly-button29");
+
+for (var i = 0; i < bubblyButtons29.length; i++) {
+  bubblyButtons29[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons30 = document.getElementsByClassName("bubbly-button30");
+
+for (var i = 0; i < bubblyButtons30.length; i++) {
+  bubblyButtons30[i].addEventListener('click', animateButton, false);
+}
+
+var bubblyButtons21 = document.getElementsByClassName("bubbly-button21");
+
+for (var i = 0; i < bubblyButtons21.length; i++) {
+  bubblyButtons21[i].addEventListener('click', animateButton, false);
+}
